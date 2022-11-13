032d685c1631e5b08002c4c693c1a22f2c6795b0
GoktanBattal <goktanbattal@gmail.com>
2022-11-13 21:12:11 +0300

    header is done

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,21 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  const headerElem = document.createElement('div');
+  headerElem.classList.add('header');
+  const dateElem = document.createElement('span');
+  dateElem.classList.add('date');
+  dateElem.textContent = date;
+  const titleElem = document.createElement('h1');
+  titleElem.textContent = title;
+  const tempElem = document.createElement('span');
+  tempElem.classList.add('temp');
+  tempElem.textContent = temp;
+  headerElem.appendChild(dateElem);
+  headerElem.appendChild(titleElem);
+  headerElem.appendChild(tempElem);
+  return headerElem;
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +35,10 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  const theHead = Header('Twitter is sold', '2022-11-13', 'template');
+  const theContent  = document.querySelector(selector);
+  theContent.appendChild(theHead);
 
   // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
   // but it can also take in a variable (ie querySelector(selector))
